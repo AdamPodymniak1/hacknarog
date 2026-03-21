cffd9bf9c46285a69e0b0a16b938d8b4a75640c9
AdamPodymniak1 <kamilgrzecznarowski@gmail.com>
2026-03-21 18:41:18 +0100

    added backend

--- most-basic-template/backend/main.py
+++
@@ -1,9 +0,0 @@
-from django.http import NinjaAPI
-from ninja import NinjaAPI
-from ninja.security import HttpBearer
-
-api = NinjaAPI()
-
-@api.get("/hello")
-def hello(request):
-    return {"message": "Hello from Django Ninja!"}
