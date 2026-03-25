dc0cc8f46ea03af7a768351658ef28f8c9b71116
AdamPodymniak1 <kamilgrzecznarowski@gmail.com>
2026-03-25 15:26:11 +0100

    fixed CORS

--- login/frontend/src/App.jsx
+++ login/frontend/src/App.jsx
@@ -89,7 +89,7 @@ function Dashboard() {
 
   return (
     <div className="p-8 text-center">
-      <h1 className="text-3xl font-bold">Welcome to the Dashboard 🍻</h1>
+      <h1 className="text-3xl font-bold">Welcome to the Dashboard!!!</h1>
       <button onClick={logout} className="mt-4 bg-red-500 text-white px-4 py-2 rounded">Logout</button>
     </div>
   );
